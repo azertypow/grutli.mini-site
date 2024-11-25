7bfefa8a5ed5af6ec782d1bd7a18b956009a061c
azertypow <nico@villa1203.ch>
2024-11-26 00:48:47 +0100

    fix: baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/content'],
   ssr: false,
+  app: {
+    baseURL: '/grutli.mini-site/',
+  },
   content: {
     experimental: {
       clientDB: true
