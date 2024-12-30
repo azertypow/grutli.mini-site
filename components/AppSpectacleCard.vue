7c73f2cdd889c287aed46883056286910a82d2af
azertypow <nico@villa1203.ch>
2024-12-30 15:33:39 +0100

    feat: get date from ticket api service

---
+++ components/AppSpectacleCard.vue
@@ -0,0 +1,127 @@
+<template>
+  <nuxt-link class="v-app-spectacle-card"
+             :to="to"
+  >
+    <h2 class="v-app-spectacle-card__item__title app-font-h4 app-font-align-center">{{ title }}</h2>
+    <div class="v-app-spectacle-card__item__date app-font-small">
+      <template v-if="getDateFromTicketService">
+        <template v-if="dateFromTicketService">{{dateFromTicketService}}</template>
+        <template v-else>chargement…</template>
+      </template>
+    </div>
+    <div class="v-app-spectacle-card__item__peoples app-font-align-center"
+         v-if="peoples"
+    >
+      <div v-for="people of peoples">
+        {{ people }}
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import {apiTicketInfomaniak_fetchEvents} from "~/utlis/apiTicketInfomaniak";
+
+interface Props {
+    to: string
+    title: string
+    peoples?: string[]
+    date?:string
+    getDateFromTicketService?: boolean
+    eventTitle?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+    getDateFromTicketService: false
+})
+
+const dateFromTicketService: Ref<string | null> = ref(null)
+
+onMounted(() => {
+    setDateToShow().then(value => {
+        dateFromTicketService.value = value
+    })
+})
+
+async function setDateToShow() {
+    if(! props.getDateFromTicketService) return null
+    if(! props.eventTitle) {
+        console.error('props.eventTitle is necessary if you set props.getDateFromTicketService to TRUE')
+        return null
+    }
+
+    if(props.getDateFromTicketService && props.date) {
+        console.error('if you give "date" prop value and set "getDateFromTicketService" prop value tu "true", "getDateFromTicketService" erase "date" prop value. Don_t use "getDateFromTicketService" to true with a value in "date" property.')
+    }
+
+    const eventsFromTicketService = await apiTicketInfomaniak_fetchEvents({
+        search: props.eventTitle
+    })
+
+    const firstDate = new Date(eventsFromTicketService[0].date).toLocaleDateString('fr-FR', {
+        day: 'numeric',
+        month: 'long',
+    })
+
+    const lastDateString = eventsFromTicketService.at(-1)?.date
+
+    const lastDate = lastDateString ? new Date(lastDateString).toLocaleDateString('fr-FR', {
+            day: 'numeric',
+            month: 'long',
+        })
+        : ''
+
+    return `${firstDate} - ${lastDate}`
+
+}
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-app-spectacle-card {
+  display: flex;
+  align-items: center;
+  border: none;
+  aspect-ratio: initial;
+  background-image: url('/s.jpg');
+  background-size: 80rem auto;
+  box-sizing: border-box;
+  padding: var(--app-gutter);
+  border-radius: 1rem;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  position: relative;
+  height: calc( (100vh - var(--app-header-height) - var(--v-audio-player-header-height)) / 2 - var(--app-gutter-xl) * 2);
+
+  > * {
+    width: 100%;
+  }
+
+  .nuxt-watch-falk-is-active & {
+    background: white;
+    color: black;
+    border: solid 1px;
+  }
+}
+
+.v-app-spectacle-card__item__date {
+  position: absolute;
+  top: var(--app-gutter-xl);
+  left: var(--app-gutter-xl);
+  width: calc(100% - var(--app-gutter-xl));
+}
+
+.v-app-spectacle-card__item__peoples {
+  display: block;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,22 @@
       <div class="v-index__list">
         <template v-for="value of siteInfo?.spectacles.slice(0, 3)">
           <div class="v-index__item">
-            <nuxt-link :to="'/spectacle/' + value[0]">
-              <h2 class="v-index__item__title app-font-h4 app-font-align-center">{{value[1].title}}</h2>
-              <div class="v-index__item__date app-font-small">
-                {{value[1].headerDate}}
-              </div>
-              <div class="v-index__item__peoples app-font-align-center">
-                <div v-for="people of value[1].peoples">
-                  {{people}}
-                </div>
-              </div>
-            </nuxt-link>
+            <AppSpectacleCard
+                    :to="'/spectacle/' + value.pageContent.slug"
+                    :title="value.pageContent.content.title"
+                    :peoples="['premier', 'second']"
+                    :get-date-from-ticket-service="true"
+                    :event-title="value.pageContent.content.eventtitle"
+            />
           </div>
         </template>
 
         <template v-for="value of pageToShowInHome">
           <div class="v-index__item">
-            <nuxt-link :to="'/' + value.pageContent.slug">
-              <h2 class="v-index__item__title app-font-align-center app-font-h4">{{value.pageContent.content.title}}</h2>
-              <div class="v-index__item__date app-font-small">
-              </div>
-            </nuxt-link>
+            <AppSpectacleCard
+                    :to="'/' + value.pageContent.slug"
+                    :title="value.pageContent.content.title"
+            />
           </div>
         </template>
       </div>
@@ -38,6 +33,7 @@
 <script setup lang="ts">
 import {_routes} from "~/utlis/spectacle/_routes";
 import type {PageSimple, SiteInfo} from "~/utlis/ApiCmsTypes";
+import AppSpectacleCard from "~/components/AppSpectacleCard.vue";
 
 const pages = _routes
 
@@ -82,34 +78,6 @@ const pageToShowInHome: ComputedRef<PageSimple[]> = computed(() => {
     width: 100%;
   }
 
-  a {
-    display: flex;
-    align-items: center;
-    border: none;
-    aspect-ratio: initial;
-    background-image: url('/s.jpg');
-    background-size: 80rem auto;
-    box-sizing: border-box;
-    padding: var(--app-gutter);
-    border-radius: 1rem;
-    flex-wrap: nowrap;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-    position: relative;
-    height: calc( (100vh - var(--app-header-height) - var(--v-audio-player-header-height)) / 2 - var(--app-gutter-xl) * 2);
-
-    > * {
-      width: 100%;
-    }
-
-    .nuxt-watch-falk-is-active & {
-      background: white;
-      color: black;
-      border: solid 1px;
-    }
-  }
-
   &:nth-child(-n+3) {
     max-width: 30rem;
     width: calc(100% / 3);
@@ -136,16 +104,5 @@ const pageToShowInHome: ComputedRef<PageSimple[]> = computed(() => {
       background-position: -7rem 60rem;
     }
   }
-}
-
-.v-index__item__date {
-  position: absolute;
-  top: var(--app-gutter-xl);
-  left: var(--app-gutter-xl);
-  width: calc(100% - var(--app-gutter-xl));
-}
-
-.v-index__item__peoples {
-  display: block;
 }
 </style>
