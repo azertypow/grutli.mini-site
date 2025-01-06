0636711ff40e81c5f5db8f9361d848c469f3dd19
azertypow <nico@villa1203.ch>
2025-01-06 10:03:18 +0100

    fix: premier second -> peoples

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
             <AppSpectacleCard
                     :to="'/spectacle/' + value.pageContent.slug"
                     :title="value.pageContent.content.title"
-                    :peoples="['premier', 'second']"
+                    :peoples="[]"
                     :get-date-from-ticket-service="true"
                     :event-title="value.pageContent.content.eventtitle"
             />
