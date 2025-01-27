f1bd2dc2f26473a85f0222ec98991688e85636f2
azertypow <nico@villa1203.ch>
2025-01-27 09:16:09 +0100

    feat: format date function

---
+++ utlis/formatDate.ts
@@ -0,0 +1,21 @@
+export function formatDate(stringDate: string): string {
+
+    const startDate = new Date(stringDate)
+
+    const options: Intl.DateTimeFormatOptions = {
+        month: 'long',
+        day: 'numeric'
+    }
+    const formatter = new Intl.DateTimeFormat('fr-FR', options)
+
+    return formatter.format(startDate)
+}
+
+export function formatDateStartAndDateEndToString(startDate: string, endDate: string): string {
+    const startDateString = formatDate(startDate)
+    const endDateString = formatDate(endDate)
+
+    if (startDateString === endDateString) return startDateString
+
+    return `${startDateString} - ${endDateString}`
+}
