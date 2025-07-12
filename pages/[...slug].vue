cbd74ae7ee8995d44d61b4862026f38abf51c099
azertypow <nico@villa1203.ch>
2025-07-12 16:22:00 +0200

    feat: unify icon for subpage

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -16,7 +16,15 @@
         <nuxt-link class="v-slug__children-link__link"
                    v-for="childLink of childrenDetailsForNavLinks"
                    :href=" '/' + childLink.pageContent.uri"
-        >{{childLink.pageContent.content.title}}</nuxt-link>
+        >
+          <span>{{ childLink.pageContent.content.title }}</span>
+          <svg xmlns="http://www.w3.org/2000/svg"
+               height="1rem"
+               width="1rem"
+               viewBox="0 -960 960 960" fill="black">
+            <path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+          </svg>
+        </nuxt-link>
       </div>
 
       <template v-if="pageData">
@@ -225,7 +233,15 @@
             <nuxt-link class="v-slug__children-link__link"
                        v-for="childLink of childrenDetailsForNavLinks"
                        :href=" '/' + childLink.pageContent.uri"
-            >{{childLink.pageContent.content.title}}</nuxt-link>
+            >
+              <span>{{ childLink.pageContent.content.title }}</span>
+              <svg xmlns="http://www.w3.org/2000/svg"
+                   height="1rem"
+                   width="1rem"
+                   viewBox="0 -960 960 960" fill="black">
+                <path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+              </svg>
+            </nuxt-link>
           </div>
 
           <template v-if="pageData">
@@ -358,7 +374,15 @@
             <nuxt-link class="v-slug__children-link__link"
                        v-for="childLink of childrenDetailsForNavLinks"
                        :href=" '/' + childLink.pageContent.uri"
-            >{{childLink.pageContent.content.title}}</nuxt-link>
+            >
+              <span>{{ childLink.pageContent.content.title }}</span>
+              <svg xmlns="http://www.w3.org/2000/svg"
+                   height="1rem"
+                   width="1rem"
+                   viewBox="0 -960 960 960" fill="black">
+                <path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+              </svg>
+            </nuxt-link>
           </div>
 
 
@@ -598,7 +622,7 @@ async function get_childrenDetailsForNavLinks(value: ApiSimplePage | null): Prom
 }
 
 .v-slug__children-link__link {
-  display: block;
+  display: flex;
   width: 100%;
   box-sizing: border-box;
   padding: 1rem;
@@ -606,7 +630,17 @@ async function get_childrenDetailsForNavLinks(value: ApiSimplePage | null): Prom
   backdrop-filter: var(--app-bg-glow-filter);
   border-radius: 1rem;
   text-align: center;
-  //text-decoration: none;
+  text-decoration: none;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: var(--app-gutter);
+
+  > svg {
+    display: block;
+    fill: currentColor;
+  }
 }
 
 .v-slug__item {
--- pages/diffusion/index.vue
+++ pages/diffusion/index.vue
@@ -21,8 +21,9 @@
               {{children.pageContent.content.title}}
           </span>
           <svg xmlns="http://www.w3.org/2000/svg"
-               style="vertical-align: bottom; margin-left: .5rem;"
-               height=".75rem" viewBox="0 -960 960 960" width=".75rem" fill="black"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
+               height ="1rem"
+               width  ="1rem"
+               viewBox="0 -960 960 960" fill="black"><path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
         </nuxt-link>
       </div>
     </template>
@@ -172,6 +173,16 @@ async function get_childrenDetailsForNavLinks(value: ApiSimplePage | null): Prom
     text-decoration: none;
     text-align: center;
     padding-bottom: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: var(--app-gutter);
+
+    > svg {
+      display: block;
+      fill: currentColor;
+    }
   }
 }
 
