da41f40840d12ac4646f740ffbbc178171eb8d72
azertypow <nico@villa1203.ch>
2025-11-19 15:04:08 +0100

    feat: text color selection  header spectacle

--- components/AppSpectacleSlugInfo.vue
+++ components/AppSpectacleSlugInfo.vue
@@ -35,18 +35,16 @@
         </div>
       </div>
 
-      <div class="v-app-spectacle-slug-info__item__info app-font-small"
-           v-if="event_info"
-      >
-        <div class="v-app-spectacle-slug-info__item__info__text">{{ event_info }}</div>
-      </div>
-
-
       <div class="v-app-spectacle-slug-info__coll__text-content__date_and_place">
         <div v-if="first_and_last_date"
              style="text-transform: capitalize;"
         >{{ first_and_last_date }}</div>
         <div>{{ place_name }}</div>
+        <div class="v-app-spectacle-slug-info__item__info app-font-small"
+             v-if="event_info"
+        >
+          <div class="v-app-spectacle-slug-info__item__info__text">{{ event_info }}</div>
+        </div>
       </div>
       <h1 class="v-app-spectacle-slug-info__coll__text-content__title app-font-align-center app-font-h1">
         {{ page_data?.pageContent.content.title }}</h1>
@@ -107,10 +105,10 @@ const props = defineProps<{
 }
 
 .v-app-spectacle-slug-info__item__info {
-  display: block;
+  display: flex;
+  justify-content: center;
   width: 100%;
   text-align: center;
-  background-color: var(--app-color-primary);
   margin-top: 1rem;
   margin-bottom: 0;
   padding: .25rem var(--app-gutter-xl);
@@ -123,10 +121,18 @@ const props = defineProps<{
   }
 }
 
+.v-app-spectacle-slug-info__item__info__text {
+  background-color: var(--app-color-primary);
+  padding: .15rem .5rem;
+  border-radius: 1rem;
+  font-variation-settings: "slnt" 0, "wght" 600;
+  margin-top: -.75em;
+}
+
 .v-app-spectacle-slug-info__coll__header__ticket {
   display: flex;
   flex-direction: column;
-  gap: var(--app-gutter-xl);
+  gap: var(--app-gutter);
   align-items: flex-end;
 
   a {
